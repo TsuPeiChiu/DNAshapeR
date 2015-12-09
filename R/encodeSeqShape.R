386e505994b2dfcac19b7741be9cca35f4e8a4ee
TsuPeiChiu <tsupeich@usc.edu>
2015-12-09 10:57:40 -0800

    Added normalization function

--- R/encodeSeqShape.R
+++ R/encodeSeqShape.R
@@ -22,7 +22,7 @@
 #' machine learning method.
 #'
 #'
-#' @usage encodeSeqShape(fastaFileName, shapeMatrix, featureNames)
+#' @usage encodeSeqShape(fastaFileName, shapeMatrix, featureNames, normalization)
 #'
 #' @param fastaFileName A character name of the input fasta format file,
 #' including full path to file if it is located outside the current working
@@ -32,6 +32,8 @@
 #' sequence and shape parameters. The parameters can be any combination of
 #' "k-mer", "n-shape", "n-MGW", "n-ProT", "n-Roll", "n-HelT" (k, n are
 #' integers)
+#' @param normalization A boolean value indicate whether to perform
+#' normalization
 #' @return featureVector A matrix containing encoded features. Sequence
 #' feature is represented as binary numbers, while shape feature is
 #' represented as continuous numbers.
@@ -46,7 +48,7 @@
 #'
 #' @export encodeSeqShape
 
-encodeSeqShape <- function( fastaFileName, shapeMatrix, featureNames){
+encodeSeqShape <- function( fastaFileName, shapeMatrix, featureNames, normalization = TRUE){
 # Generate sequence and shape feature matrix
 #
 # Args:
@@ -67,30 +69,55 @@ encodeSeqShape <- function( fastaFileName, shapeMatrix, featureNames){
         switch( featureName[2],
             mer = { featureVector <- cbind( featureVector,
                     encodeKMerSeq( as.numeric( featureName[1] ), ds ) ) },
-            #hbond = { featureVector <- cbind( featureVector,
-            #       encodeKMerHbond( as.numeric( featureName[1] ), ds ) ) },
             MGW = { featureVector <- cbind( featureVector,
-                    encodeNstOrderShape( as.numeric( featureName[1] ),
-                                         shapeMatrix$MGW ) ) },
+                        normalizeShape(
+                            encodeNstOrderShape( as.numeric( featureName[1] ),
+                            shapeMatrix$MGW, "MGW" ), as.numeric( featureName[1] ),
+                            "MGW" , normalization) )
+            },
             ProT = { featureVector <- cbind( featureVector,
-                    encodeNstOrderShape( as.numeric( featureName[1] ),
-                                         shapeMatrix$ProT ) ) },
+                        normalizeShape(
+                            encodeNstOrderShape( as.numeric( featureName[1] ),
+                            shapeMatrix$ProT, "ProT" ), as.numeric( featureName[1] ),
+                            "ProT" , normalization) )
+            },
             Roll = { featureVector <- cbind( featureVector,
-                    encodeNstOrderShape( as.numeric( featureName[1] ),
-                                         shapeMatrix$Roll ) ) },
+                        normalizeShape(
+                            encodeNstOrderShape( as.numeric( featureName[1] ),
+                            shapeMatrix$Roll, "Roll" ), as.numeric( featureName[1] ),
+                            "Roll" , normalization) )
+            },
+
             HelT = { featureVector <- cbind( featureVector,
-                    encodeNstOrderShape( as.numeric( featureName[1] ),
-                                         shapeMatrix$HelT ) ) },
+                        normalizeShape(
+                            encodeNstOrderShape( as.numeric( featureName[1] ),
+                            shapeMatrix$HelT, "HelT" ), as.numeric( featureName[1] ),
+                            "HelT" , normalization) )
+            },
+
+
             shape = {
                     featureVector <- cbind( featureVector,
-                    encodeNstOrderShape( as.numeric( featureName[1] ),
-                                         shapeMatrix$MGW ),
-                    encodeNstOrderShape( as.numeric( featureName[1] ),
-                                         shapeMatrix$ProT ),
-                    encodeNstOrderShape( as.numeric( featureName[1] ),
-                                         shapeMatrix$Roll ),
-                    encodeNstOrderShape( as.numeric( featureName[1] ),
-                                         shapeMatrix$HelT ) )
+                        normalizeShape(
+                            encodeNstOrderShape( as.numeric( featureName[1] ),
+                            shapeMatrix$MGW, "MGW" ), as.numeric( featureName[1] ),
+                            "MGW" , normalization ),
+
+                        normalizeShape(
+                            encodeNstOrderShape( as.numeric( featureName[1] ),
+                            shapeMatrix$ProT, "ProT" ), as.numeric( featureName[1] ),
+                            "ProT" , normalization ),
+
+                        normalizeShape(
+                            encodeNstOrderShape( as.numeric( featureName[1] ),
+                            shapeMatrix$Roll, "Roll" ), as.numeric( featureName[1] ),
+                            "Roll" , normalization ),
+
+                        normalizeShape(
+                            encodeNstOrderShape( as.numeric( featureName[1] ),
+                            shapeMatrix$HelT, "HelT" ), as.numeric( featureName[1] ),
+                            "HelT" , normalization )
+                    )
             }
         )
     }
@@ -159,12 +186,13 @@ encodeKMerSeq <- function( k, dnaStringSet ){
 #
 # @param n A number indicating n-st order shape encoding
 # @param shapeMatrix A matrix containing DNAshape prediction result
-# @return  featureVector A matrix containing encoded features. shape feature is
+# @param shapeType A character name of shape (MGW, Roll, ProT, HelT) features
+# @return featureVector A matrix containing encoded features. shape feature is
 # represented as continuous numbers
 # @author Tsu-Pei Chiu
 #
 
-encodeNstOrderShape <- function( n, shapeMatrix ){
+encodeNstOrderShape <- function( n, shapeMatrix, shapeType ){
     # trim end columns with NA
     shapeMatrix[ is.na( shapeMatrix ) ] <- 0
        for( i in c( ncol( shapeMatrix ), ncol( shapeMatrix )-1, 2, 1 ) ){
@@ -180,8 +208,7 @@ encodeNstOrderShape <- function( n, shapeMatrix ){
     }else{
         m <- ncol( shapeMatrix )
         # normalization
-        shapeMatrix <- ( shapeMatrix - min( shapeMatrix ) ) /
-          ( max( shapeMatrix ) - min( shapeMatrix ))
+        shapeMatrix <- normalizeShape( shapeMatrix, 1, shapeType, TRUE )
 
         for ( i in 1 : ( m-n+1 )){
             feature <- shapeMatrix[, i]
@@ -196,43 +223,66 @@ encodeNstOrderShape <- function( n, shapeMatrix ){
 }
 
 
-## encode Hbond
-##
-## @usage encodeKmerHbond (filepath)
-## @param k k-mer sequence
-## @param dnaStringSet
-## @return featureVector
+# normalized n-st order shape features
+#
+# @usage normalizeShape(featureVector, thOrder, shapeType, normalization)
+#
+# @param featureVector A matrix containing encoded features.
+# @param thOrder A number indicating n-st order shape encoding
+# @param shapeType A character name of shape (MGW, Roll, ProT, HelT) features
+# @param normalization
+# @return featureVector A matrix containing encoded features. shape feature is
+# represented as continuous numbers
+# @author Tsu-Pei Chiu
+#
+normalizeShape <- function( featureVector, thOrder, shapeType, normalization ){
+    minMGW <- 2.85
+    maxMGW <- 6.2
+    minProT <- -16.51
+    maxProT <- -0.03
+    minRoll <- -8.57
+    maxRoll <- 8.64
+    minHelT <- 30.94
+    maxHelT <- 38.05
 
-#encodeKmerHbond <- function ( k, dnaStringSet ){
-#  # create a lookup table
-#  k <- 1
-#  lookupTable <- rbind( c(1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,1),
-#                        c(0,0,1,0,0,1,0,0,1,0,0,0,1,0,0,0),
-#                        c(1,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0),
-#                        c(0,0,0,1,1,0,0,0,0,1,0,0,1,0,0,0) )
-#  row.names( lookupTable ) <- mkAllStrings( c( "A", "C", "G", "T" ), k )
+    if ( normalization  ){
+        if( thOrder == 1){
+            switch( shapeType,
+                    MGW = {
+                        featureVector <- normalize( featureVector, maxMGW, minMGW )
+                    },
+                    ProT = {
+                        featureVector <- normalize( featureVector, maxProT, minProT )
+                    },
+                    Roll = {
+                        featureVector <- normalize( featureVector, maxRoll, minRoll )
+                    },
+                    HelT = {
+                        featureVector <- normalize( featureVector, maxHelT, minHelT )
+                    }
+                )
+
+        }else{
+            featureVector <- normalize( featureVector, max(featureVector), min(featureVector) )
+        }
+    }
+
+    return ( featureVector )
+}
+
+
+# Min-Max normalization
 #
-#  featureVector <- c()
+# @usage normalize(x, max, min)
 #
-#  for( j in 1 : length( dnaStringSet ) ){
-#    # encode k-mer feature
-#    features <- c()
-#    seq <- toString( dnaStringSet[j] )
-#    for ( j in 1 : ( nchar( seq )-k+1) ){
-#      if( is.na( match( substr( toupper( seq ), j, j+k-1),
-#          row.names( lookupTable ) ) ) ){
-#        features <- c( features, rep( 0, 16**k ) )
+# @param x A matrix containing encoded features
+# @param max A number maximum number for Min-Max Normalization
+# @param min A number minimum number for Min-Max Normalization
+# @return  featureVector A matrix containing encoded features. shape feature is
+# represented as continuous numbers
+# @author Tsu-Pei Chiu
 #
-#      }else{
-#        features <- c( features,
-#       lookupTable[ substr( toupper( seq ), j, j+k-1), ] )
-#      }
-#    }
-#
-#    featureVector <- rbind( featureVector, features )
-#  }
-#  row.names( featureVector ) <- names( dnaStringSet )
-#
-#  return ( featureVector )
-#
-#}
+normalize <- function( x, max, min ){
+  return ( (x-min)/(max-min) )
+}
+
