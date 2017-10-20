ee9944538b7bc16f003cd29afa97a1f4d6ccf059
TsuPeiChiu <tsupeich@usc.edu>
2017-10-20 08:59:56 -0700

    Fixed conflicts in version change

--- src/SNP.h
+++ src/SNP.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "common.h"
 #include "utilities.h"
 #include "map.h"
@@ -8,14 +7,4 @@ void SNP_distribution(DNA_to_properties&);
 void predict_mgw_to_vector(DNA_to_properties&,std::string,double_vector&);
 
 double pairwise_Euclidean(double_vector&,double_vector&);
-=======
-#include "common.h"
-#include "utilities.h"
-#include "map.h"
 
-void SNP_distribution(DNA_to_properties&);
-
-void predict_mgw_to_vector(DNA_to_properties&,std::string,double_vector&);
-
-double pairwise_Euclidean(double_vector&,double_vector&);
->>>>>>> upstream/master
